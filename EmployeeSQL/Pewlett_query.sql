466a0e1cd4fb2a6f82580ff107d836f81c205c16
Kopano Majara <kops4jc@gmail.com>
2024-07-31 12:52:36 -0400

    first couple of actions

---
+++ EmployeeSQL/Pewlett_query.sql
@@ -0,0 +1,15 @@
+﻿---------------------SQL-QUERY------------------
+
+--List each employee's number,last name, first name,
+--sex and salary.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+LEFT JOIN salaries
+ON employees.emp_no = salaries.emp_no;
+
+--List the first name,last name and hire date for
+--employees hired in 1986.
+SELECT  first_name, last_name , hire_date
+FROM employees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+
